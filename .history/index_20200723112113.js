e8869e1d5f8680ade450d82e535606db220c2d10
Charlie Pelton <charliejp0311@gmail.com>
2020-07-23 11:31:15 -0500

    Done.

--- .history/index_20200723112113.js
+++ .history/index_20200723112113.js
@@ -8,24 +8,24 @@ function moveDodgerLeft() {
     if (left > 0) {
         dodger.style.left = `${left - 1}px`;
     }
-};
+}
 
 function moveDodgerRight() {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
+    let rightNumbers = dodger.style.right.replace("px", "");
+    let right = parseInt(leftNumbers, 10);
 
-    if (left > 0) {
-        dodger.style.left = `${left + 1}px`;
-    };
-};
+    if (right > 0) {
+        dodger.style.right = `${tight - 1}px`;
+    }
+}
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
-    };
+    }
 });
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowRight") {
         moveDodgerRight();
-    };
+    }
 });
